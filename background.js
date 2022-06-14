6b98aea61522422fbea1f03e4c5e1fae0ab63ab5
Taehwan <15leetaehwan@gmail.com>
2022-06-14 13:52:55 -0700

    App can now switch from focus to break timer automatically

--- background.js
+++ background.js
@@ -1,52 +1,104 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action == 'startTimer'){
-        startTimer(request.focusTime, request.focusTimeSeconds);
-    } else if (request.action == 'pauseTimer'){
-        pauseTimer();
-    } else if (request.action == 'stopTimer'){
-        stopTimer(request.focusTime, request.focusTimeSeconds);
-    } else if (request.action=='loadTime') {
-        loadTime();
-    }
-})
+  if (request.action == 'startTimer') {
+    startTimer(
+      request.focusTime,
+      request.focusTimeSeconds,
+      request.breakTime,
+      request.breakTimeSeconds
+    );
+  } else if (request.action == 'pauseTimer') {
+    pauseTimer();
+  } else if (request.action == 'stopTimer') {
+    stopTimer(request.focusTime, request.focusTimeSeconds);
+  } else if (request.action == 'loadTime') {
+    loadTime();
+  }
+});
 
 let everySecond;
 let minutes = 25;
 let seconds = 0;
-chrome.storage.local.set({paused: false})
+chrome.storage.local.set({ paused: false });
 
-let startTimer = (focusTime, focusTimeSeconds) => {
-    let paused = chrome.storage.local.get('paused', ({paused})=> {
-        if (!paused) {
-            minutes = focusTime;
-            seconds = focusTimeSeconds;
+let startTimer = (focusTime, focusTimeSeconds, breakTime, breakTimeSeconds) => {
+  let paused = chrome.storage.local.get('paused', ({ paused }) => {
+    if (!paused) {
+      minutes = focusTime;
+      seconds = focusTimeSeconds;
+    }
+  });
+  chrome.storage.local.set({ paused: false });
+  pauseTimer();
+  everySecond = setInterval(() => {
+    seconds -= 1;
+    if (seconds < 0) {
+      seconds = 59;
+      minutes -= 1;
+    }
+
+    if (minutes == 0 && seconds == 0) {
+      chrome.storage.local.get('breakTimeStatus', ({ breakTimeStatus }) => {
+        if (!breakTimeStatus) {
+          minutes = breakTime;
+          seconds = breakTimeSeconds;
+          chrome.storage.local.set({ breakTimeStatus: true });
+          playSound('bubbles');
+        } else {
+          minutes = focusTime;
+          seconds = focusTimeSeconds;
+          chrome.storage.local.set({ breakTimeStatus: false });
+
+          playSound('bubbleBreak');
         }
+      });
+    }
+
+    chrome.runtime.sendMessage({
+      action: 'timer',
+      minutes: minutes,
+      seconds: seconds,
     });
-    chrome.storage.local.set({paused: false});
-    pauseTimer();
-    everySecond = setInterval(() => {
-        
-        seconds -= 1;
-        if (seconds < 0) {
-            seconds = 59;
-            minutes -= 1;
-        }
-        chrome.runtime.sendMessage({ action: 'timer', minutes: minutes, seconds: seconds})
-    }, 1000);
-}
+  }, 1000);
+};
 
 let pauseTimer = () => {
-    chrome.storage.local.set({paused: true});
+  chrome.storage.local.set({ paused: true });
   clearInterval(everySecond);
 };
 
 let stopTimer = (focusTime, focusTimeSeconds) => {
-    pauseTimer();
+  pauseTimer();
   minutes = focusTime;
   seconds = focusTimeSeconds;
-  chrome.runtime.sendMessage({ action: 'timer', minutes: minutes, seconds: seconds})
+  chrome.runtime.sendMessage({
+    action: 'timer',
+    minutes: minutes,
+    seconds: seconds,
+  });
 };
 
 let loadTime = () => {
-    chrome.runtime.sendMessage({ action: 'timer', minutes: minutes, seconds: seconds})
-}
+  chrome.runtime.sendMessage({
+    action: 'timer',
+    minutes: minutes,
+    seconds: seconds,
+  });
+};
+
+let playSound = (source) => {
+  let url = chrome.runtime.getURL('breakStart.html');
+
+  // set this string dynamically in your code, this is just an example
+  // this will play success.wav at half the volume and close the popup after a second
+  url += `?volume=0.5&src=Sounds/${source}.wav&length=5000`;
+
+  chrome.windows.create({
+    type: 'popup',
+    focused: false,
+    top: 1,
+    left: 1,
+    height: 1,
+    width: 1,
+    url,
+  });
+};
--- script.js
+++ script.js
@@ -5,73 +5,121 @@ let resetButton = document.getElementById('stop-button');
 let minutesDisplay = document.getElementById('minutes');
 let secondsDisplay = document.getElementById('seconds');
 
-let focusTime, focusTimeSeconds, breakTime, breakTimeSeconds;
+let focusTime = 25,
+  focusTimeSeconds = 0;
+let breakTime = 5,
+  breakTimeSeconds = 0;
 let paused = false;
 
-let setFocusTime = () => {
-    if (document.getElementById('focus-time').value.slice(0, 2)) {
-        focusTime = document.getElementById('focus-time').value.slice(0, 2)
-        focusTimeSeconds = document.getElementById('focus-time').value.slice(3)
-    } else{
-        focusTime = 25;
-        focusTimeSeconds=0;
-    }
-}
+chrome.storage.local.set({ breakTimeStatus: false });
 
-let setBreakTime = () => {
-    if (document.getElementById('break-time').value.slice(0, 2)) {
-        breakTime = document.getElementById('break-time').value.slice(0, 2)
-        breakTimeSeconds = document.getElementById('break-time').value.slice(3)
-    } else{
-        breakTime = 25;
-        breakTimeSeconds=0;
+let setTimes = () => {
+  return new Promise((resolve, reject) => {
+    if (document.getElementById('focus-time').value.slice(0, 2)) {
+      focusTime = document.getElementById('focus-time').value.slice(0, 2);
+      focusTimeSeconds = document.getElementById('focus-time').value.slice(3);
+      console.log(focusTimeSeconds);
+    } else {
+      focusTime = 25;
+      focusTimeSeconds = 0;
     }
-}
+    if (document.getElementById('break-time').value.slice(0, 2)) {
+      breakTime = document.getElementById('break-time').value.slice(0, 2);
+      breakTimeSeconds = document.getElementById('break-time').value.slice(3);
+    } else {
+      breakTime = 25;
+      breakTimeSeconds = 0;
+    }
+    resolve();
+  });
+};
 
 closeButton.addEventListener('click', () => window.close());
 startButton.addEventListener('click', () => start());
 pauseButton.addEventListener('click', () => pause());
 resetButton.addEventListener('click', () => stop());
 
-let start = () => {
-  if(!paused){
-    setFocusTime();
+let start = async () => {
+  hide(startButton);
+  show(pauseButton);
+  show(resetButton);
+
+  if (!paused) {
+    await setTimes();
   }
-  chrome.runtime.sendMessage({ action: 'startTimer', focusTime: focusTime, focusTimeSeconds: focusTimeSeconds})
+  chrome.runtime.sendMessage({
+    action: 'startTimer',
+    focusTime: focusTime,
+    focusTimeSeconds: focusTimeSeconds,
+    breakTime: breakTime,
+    breakTimeSeconds: breakTimeSeconds,
+  });
   document.getElementById('start-audio').play();
-}
+};
 
 let pause = () => {
+  hide(pauseButton);
+  show(startButton);
+  show(resetButton);
+
   paused = true;
-  chrome.runtime.sendMessage({action: 'pauseTimer'});
+  chrome.runtime.sendMessage({ action: 'pauseTimer' });
   document.getElementById('pause-audio').play();
-}
+};
 
 let stop = () => {
-  setFocusTime();
-  chrome.runtime.sendMessage({action: 'stopTimer' , focusTime: focusTime, focusTimeSeconds: focusTimeSeconds});
+  show(startButton);
+  hide(pauseButton);
+  hide(resetButton);
+  setTimes();
+  chrome.runtime.sendMessage({
+    action: 'stopTimer',
+    focusTime: focusTime,
+    focusTimeSeconds: focusTimeSeconds,
+  });
   document.getElementById('stop-audio').play();
-}
+};
 
 let timer = (minutes, seconds) => {
-  
   minutesDisplay.innerText = formatTime(minutes);
   secondsDisplay.innerText = formatTime(seconds);
 };
 
-chrome.runtime.sendMessage({ action: 'loadTime'});
+let completion = () => {
+  document.getElementById('completion-audio').play();
+};
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
-  if (request.action == 'timer'){
+let breakCompletion = () => {
+  document.getElementById('break-audio').play();
+};
+
+chrome.runtime.sendMessage({ action: 'loadTime' });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action == 'timer') {
     timer(request.minutes, request.seconds);
   }
-})
+  if (request.action == 'completion') {
+    completion();
+  }
+
+  if (request.action == 'breakCompletion') {
+    breakCompletion();
+  }
+});
 
 function formatTime(x) {
-  return x >= 10 || x==='00' ? x : `0${x}`;
+  return x >= 10 || x === '00' ? x : `0${x}`;
 }
 
-// function hide(element) {
-//     element.classList.remove("active");
-//     element.classList.add("inactive");
-// }
+function hide(element) {
+  element.classList.remove('active');
+  // element.classList.add('inactive');
+  element.style.animation = '5s forwards float-away';
+}
+
+function show(element) {
+  element.classList.remove('inactive');
+  // element.classList.add('active');
+  element.style.animation = '5s forwards float-in';
+}
